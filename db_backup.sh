acb614565b51b7b9280f48467a62c0acd4621433
Shuhei Nishimura <nishimura@digitalcube.jp>
2020-03-17 17:10:46 +0900

    set default value

--- db_backup.sh
+++ db_backup.sh
@@ -1,23 +1,28 @@
+cat <<EOM
+
+Default value:
+ - change direcroty : wordpress
+ - want to put db directory : ../
+rename(bool) default → false
+ - rename === fale : file name → backup.sql
+ - rename === true : file name → backup_Y-m-d-H-M-S.sql
+
+EOM
+
 read -p "change direcroty / want to put db directory / rename > " d1 d2 d3 d4;
 
-printf '%1s\033[31m%2s\033[m\n' "your change directory → " $d1;
-printf '%1s\033[31m%2s\033[m\n' "your backup directory → " $d2;
+printf '%1s\033[31m%2s\033[m\n' "your change directory → " ${d1:='wordpress'};
+printf '%1s\033[31m%2s\033[m\n' "your backup directory → " ${d2:='../'};
 
 cd $d1;
 
-if [ $d3 = 'false' ]; then
-
-  #echo 'this is false';
+if [ ${d3:='false'} ]; then
   file_name="backup.sql";
-
 else
-
-  #echo 'this is true'
   file_name="backup_`date +%Y-%m-%d-%H-%M-%S`.sql"
-
 fi
 
-echo 'Now backup '$file_name' per hour. :)';
+echo 'Now backup file → '$file_name' per hour. :)';
 
 while true;
  do wp db export "$d2""$file_name";
