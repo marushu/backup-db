10f276a363d0763ffb85d0593838e4ed7331e9d4
Shuhei Nishimura <nishimura@digitalcube.jp>
2020-03-17 15:33:06 +0900

    change parameters

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Backup DB to selected directory every hour.
 Save backup_year_month_date_hour_minute_second.sql in the specified directory every hour.
 
-## input directory
+## parameters
 Enter the directory to be moved and the directory to be backed up.
 1. want to change directory
-2. want to put your backup file(Y-m-d-H-M-S.sql).
+1. want to put your backup file.
+1. rename(true) or not(false)
+  1. if rename is true, then file name is *backup.sql*.
+  1. if rename is false, then file name is *backup_Y-m-d-H-M-S.sql*.
--- db_backup.sh
+++ db_backup.sh
@@ -1,1 +1,25 @@
-read -p "change direcroty / want to put db directory > " d1 d2; printf '%1s\033[31m%2s\033[m\n' "your change directory → " $d1; printf '%1s\033[31m%2s\033[m\n' "your backup directory → " $d2; cd $d1; while true; do wp db export "$d2"backup_`date +%Y-%m-%d-%H-%M-%S`.sql; sleep 3600; done
+read -p "change direcroty / want to put db directory / rename > " d1 d2 d3 d4;
+
+printf '%1s\033[31m%2s\033[m\n' "your change directory → " $d1;
+printf '%1s\033[31m%2s\033[m\n' "your backup directory → " $d2;
+
+cd $d1;
+
+if [ $d3 = 'false' ]; then
+
+  #echo 'this is false';
+  file_name="backup.sql";
+
+else
+
+  #echo 'this is true'
+  file_name="backup_`date +%Y-%m-%d-%H-%M-%S`.sql"
+
+fi
+
+echo 'Now backup '$file_name' per hour. :)';
+
+while true;
+ do wp db export "$d2""$file_name";
+ sleep 3600;
+done
